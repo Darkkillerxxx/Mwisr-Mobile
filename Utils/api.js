c786f8e2e863bf9e5473071603a92aada391c7f2
Darkkillerxxx <adidabs1994@gmail.com>
2020-08-13 21:59:11 +0530

    Package Details in Progress

--- Utils/api.js
+++ Utils/api.js
@@ -36,7 +36,8 @@ const endpoint_url={
     AddUser:base_url_wealthyFox+"/AnalystRegistration/AddUser",
     GetUserPermissionSet:base_url_wealthyFox+"/AnalystRegistration/GetUserPermissionSet",
     UpsertUserPermission:base_url_wealthyFox + "/AnalystRegistration/UpsertPermission",
-    GetSubDetail:base_url_wealthyFox+"MwisrQueries/GetSubDetail"
+    GetSubDetail:base_url_wealthyFox+"MwisrQueries/GetSubDetail",
+    GetPackageDetails:base_url_wealthyFox +"AnalystQuery/GetPackageDetail"
 }
 
 /********* Normal Functions **********/
@@ -693,6 +694,13 @@ export function upsert_user_permission(authHeader,payload)
 export function get_sub_detail(authHeader,payload)
 {
   return apiCall(endpoint_url['GetSubDetail'],"POST",payload,authHeader).then(data => {
+    return JSON.parse(data)
+  }).catch(err => err)
+}
+
+export function get_pacakge_details(authHeader,payload)
+{
+  return apiCall(endpoint_url['GetPackageDetails'],"GET",payload,authHeader).then(data => {
     return JSON.parse(data)
   }).catch(err => err)
 }
